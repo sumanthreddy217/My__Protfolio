d20d042f9cadf138457bab3416819a0e58dd0481
sumanthreddy217 <sumanthreddy.k861@gmail.com>
2025-01-24 01:26:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// this js code is onlyfor mobile navbar //
+
+const toggleBtn = document.querySelector('.toggle-btn')
+const toggleBtnIcon = document.querySelector('.toggle-btn i')
+const dropDownMenu = document.querySelector('.dropdown-menu')
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
+
+    toggleBtnIcon.classList = isOpen
+    ? 'fa-solid fa-xmark'
+    : 'fa-solid fa-bars'
+    }
